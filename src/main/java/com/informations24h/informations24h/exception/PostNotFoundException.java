d21024acb1676fddded9d618fed69cdde4a841e5
Grzesiek1337 <gmiedzian@gmail.com>
2022-06-20 20:28:39 +0200

    added post not found exception class

---
+++ src/main/java/com/informations24h/informations24h/exception/PostNotFoundException.java
@@ -0,0 +1,8 @@
+package com.informations24h.informations24h.exception;
+
+@SuppressWarnings("serial")
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException() {
+
+    }
+}
