4cddf2850b0a4c00e998ccf1176b10959c30ee4c
Grzesiek1337 <gmiedzian@gmail.com>
2022-06-20 20:27:59 +0200

    added post jpa interface

---
+++ src/main/java/com/informations24h/informations24h/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.informations24h.informations24h.repository;
+
+import com.informations24h.informations24h.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
