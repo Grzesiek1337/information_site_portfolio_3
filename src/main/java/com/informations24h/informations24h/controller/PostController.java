4e7449b7882083480693aa94fa0bb9d3a6a56983
Grzesiek1337 <gmiedzian@gmail.com>
2022-06-20 20:32:48 +0200

    added empty post controller

---
+++ src/main/java/com/informations24h/informations24h/controller/PostController.java
@@ -0,0 +1,9 @@
+package com.informations24h.informations24h.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+
+}
