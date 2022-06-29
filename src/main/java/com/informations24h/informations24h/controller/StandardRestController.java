67c776ee43fabab41ad97d8b58b5bdccb7d3a061
Grzesiek1337 <gmiedzian@gmail.com>
2022-06-29 21:32:28 +0200

    log4j implementation test

--- src/main/java/com/informations24h/informations24h/controller/StandardRestController.java
+++ src/main/java/com/informations24h/informations24h/controller/StandardRestController.java
@@ -1,17 +1,21 @@
 package com.informations24h.informations24h.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @RestController
 public class StandardRestController {
 
+    private static final Logger logger = Logger.getLogger(StandardRestController.class);
 
     @GetMapping("/getAuthor")
     public String getAuthor() {
+        logger.info("Getting author at " + LocalDateTime.now());
         return "Grzegorz M";
     }
 
