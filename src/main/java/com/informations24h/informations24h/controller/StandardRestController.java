a3b266d323ec8e25ad0dedb9c717d38cb2d3aecf
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-04 18:13:29 +0200

    mapper impl

--- src/main/java/com/informations24h/informations24h/controller/StandardRestController.java
+++ src/main/java/com/informations24h/informations24h/controller/StandardRestController.java
@@ -1,6 +1,10 @@
 package com.informations24h.informations24h.controller;
 
+import com.informations24h.informations24h.dto.AuthorDto;
+import com.informations24h.informations24h.model.Author;
 import org.apache.log4j.Logger;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,20 +17,29 @@ public class StandardRestController {
 
     private static final Logger logger = Logger.getLogger(StandardRestController.class);
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @GetMapping("/getAuthor")
     public String getAuthor() {
-        logger.info("Getting author at " + LocalDateTime.now());
-        return "Grzegorz M";
+        logger.info("Author checked at " + LocalDateTime.now());
+        return convertAuthorToDto(new Author("grzesiek1988","123","ADMIN",true)).toString();
     }
 
     @GetMapping("/getDate")
     public LocalDate getDate() {
+        logger.info("The date has been checked.");
         return LocalDate.now();
     }
 
     @GetMapping("/getTime")
     public LocalTime getTime() {
+        logger.info("The time has been checked.");
         return LocalTime.now();
+    }
+
+    public AuthorDto convertAuthorToDto(Author author) {
+        return modelMapper.map(author,AuthorDto.class);
     }
 
 }
