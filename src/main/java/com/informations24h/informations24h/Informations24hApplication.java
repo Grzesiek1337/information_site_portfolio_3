fe657c2909c89d07bd0ea321917c1eee06a5e8fe
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-04 18:41:09 +0200

    added modelmapper bean

--- src/main/java/com/informations24h/informations24h/Informations24hApplication.java
+++ src/main/java/com/informations24h/informations24h/Informations24hApplication.java
@@ -1,7 +1,9 @@
 package com.informations24h.informations24h;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Informations24hApplication {
@@ -10,4 +12,8 @@ public class Informations24hApplication {
 		SpringApplication.run(Informations24hApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
