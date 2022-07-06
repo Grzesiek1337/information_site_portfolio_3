5d400762bcd1e22a861c90b685df90f308ad7d35
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-06 21:11:43 +0200

    added name and date and time models to display on the page

--- src/main/java/com/informations24h/informations24h/controller/AuthorPanelController.java
+++ src/main/java/com/informations24h/informations24h/controller/AuthorPanelController.java
@@ -1,10 +1,30 @@
 package com.informations24h.informations24h.controller;
 
+import com.informations24h.informations24h.service.AuthorService;
+import com.informations24h.informations24h.service.CurrentUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class AuthorPanelController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @ModelAttribute("author")
+    public String getAuthor(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
+        return currentUserDetails.getAuthor().getUsername();
+    }
+
+    @ModelAttribute("localDateTime")
+    public LocalDateTime getTime() {
+        return LocalDateTime.now();
+    }
 
     @GetMapping("/author-panel")
     public String getAuthorPanel() {
