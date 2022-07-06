86117f3f59a3cbeaa83532a97248922c6f0debd8
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-06 19:56:34 +0200

    added query method to find author by name

--- src/main/java/com/informations24h/informations24h/repository/AuthorRepository.java
+++ src/main/java/com/informations24h/informations24h/repository/AuthorRepository.java
@@ -2,6 +2,11 @@ package com.informations24h.informations24h.repository;
 
 import com.informations24h.informations24h.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AuthorRepository extends JpaRepository<Author,Long> {
+
+    @Query("SELECT a FROM Author a WHERE a.username = :username")
+    Author getAuthorByUsername(@Param("username") String username);
 }
