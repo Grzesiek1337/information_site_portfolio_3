e9ae5ff32b2d45c9f052b7b84629fbc4c7589810
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-06 20:08:41 +0200

    mplementation of the method that creates the admin

--- src/main/java/com/informations24h/informations24h/controller/StandardRestController.java
+++ src/main/java/com/informations24h/informations24h/controller/StandardRestController.java
@@ -2,6 +2,7 @@ package com.informations24h.informations24h.controller;
 
 import com.informations24h.informations24h.dto.AuthorDto;
 import com.informations24h.informations24h.model.Author;
+import com.informations24h.informations24h.service.AuthorService;
 import org.apache.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,8 @@ import java.time.LocalTime;
 public class StandardRestController {
 
     private static final Logger logger = Logger.getLogger(StandardRestController.class);
-
+    @Autowired
+    private AuthorService authorService;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -38,8 +40,24 @@ public class StandardRestController {
         return LocalTime.now();
     }
 
+    @GetMapping("/createAdmin")
+    public String createAdminAuthor() {
+        AuthorDto authorDto = new AuthorDto();
+        authorDto.setUsername("GrzegorzM");
+        authorDto.setPassword("123");
+        authorDto.setRole("ROLE_ADMIN");
+        authorService.encodePasswordSetEnableANdSave(convertAuthorDtoToEntity(authorDto));
+        logger.info("Admin has been created.");
+        return "success";
+    }
+
     public AuthorDto convertAuthorToDto(Author author) {
         return modelMapper.map(author,AuthorDto.class);
     }
+
+    public Author convertAuthorDtoToEntity(AuthorDto authorDto) {
+        return modelMapper.map(authorDto,Author.class);
+    }
+
 
 }
