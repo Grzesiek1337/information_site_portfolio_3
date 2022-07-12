6e3e141965e99784d5805fc4854876a8611afbbe
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-12 19:55:29 +0200

    added model attribute, all posts

--- src/main/java/com/informations24h/informations24h/controller/IndexController.java
+++ src/main/java/com/informations24h/informations24h/controller/IndexController.java
@@ -1,13 +1,20 @@
 package com.informations24h.informations24h.controller;
 
+import com.informations24h.informations24h.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postService.convertPostListToDto(postService.listAll()));
         return "index";
     }
 }
