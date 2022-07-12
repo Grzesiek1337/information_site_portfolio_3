8bbf0ff6464bf406edd302c1132c8db1dec1f419
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-12 20:22:35 +0200

    updated content

--- src/main/java/com/informations24h/informations24h/service/PostService.java
+++ src/main/java/com/informations24h/informations24h/service/PostService.java
@@ -1,20 +1,26 @@
 package com.informations24h.informations24h.service;
 
+import com.informations24h.informations24h.dto.PostDto;
 import com.informations24h.informations24h.model.Post;
 import com.informations24h.informations24h.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class PostService implements IPostService {
 
+    @Autowired
     private final PostRepository postRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public List<Post> listAll() {
@@ -23,21 +29,28 @@ public class PostService implements IPostService {
 
     @Override
     public void save(Post post) {
-
+        postRepository.save(post);
     }
 
     @Override
     public Post get(long id) {
-        return null;
+        return postRepository.findById(id).get();
     }
 
     @Override
     public void update(Post post) {
+        postRepository.save(post);
 
     }
 
     @Override
     public void delete(long id) {
+    postRepository.deleteById(id);
+    }
 
+    public List<PostDto> convertPostListToDto(List<Post> posts) {
+        return posts.stream()
+                .map(p -> modelMapper.map(p, PostDto.class))
+                .collect(Collectors.toList());
     }
 }
