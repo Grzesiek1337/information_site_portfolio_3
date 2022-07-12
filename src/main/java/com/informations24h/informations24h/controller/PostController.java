aafbb1b166b54b1efa71206d62a11288bc8a144a
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-12 20:20:49 +0200

    transfered method to post service

--- src/main/java/com/informations24h/informations24h/controller/PostController.java
+++ src/main/java/com/informations24h/informations24h/controller/PostController.java
@@ -1,18 +1,13 @@
 package com.informations24h.informations24h.controller;
 
-import com.informations24h.informations24h.dto.PostDto;
-import com.informations24h.informations24h.model.Post;
 import com.informations24h.informations24h.repository.PostArchive;
 import com.informations24h.informations24h.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.apache.log4j.Logger;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/post")
@@ -23,27 +18,17 @@ public class PostController {
     @Autowired
     private PostService postService;
     @Autowired
-    private PostArchive postArchive = new PostArchive();
-
-    @Autowired
-    private ModelMapper modelMapper;
+    private PostArchive postArchive;
 
     @GetMapping("/all")
     public String getPosts() {
         logger.info("Load all posts at " + LocalDateTime.now());
-        return postService.listAll().toString();
+        return postService.convertPostListToDto(postService.listAll()).toString();
     }
 
     @GetMapping("/archive")
     public String getArchivePosts() {
-        return convertToDtoPostList(postArchive.getPosts()).toString();
-
+        logger.info("Load all archive posts at " + LocalDateTime.now());
+        return postService.convertPostListToDto(postArchive.getPosts()).toString();
     }
-
-    public List<PostDto> convertToDtoPostList(List<Post> posts) {
-        return posts.stream()
-                .map(p -> modelMapper.map(p, PostDto.class))
-                .collect(Collectors.toList());
-    }
-
 }
