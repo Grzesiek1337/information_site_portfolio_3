6c9c3c4f0845e982d5f4967bca5fe2014621d1f3
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-16 15:25:27 +0200

    added Category class

---
+++ src/main/java/com/informations24h/informations24h/model/Category.java
@@ -0,0 +1,15 @@
+package com.informations24h.informations24h.model;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Category {
+
+    private String categoryName;
+}
