f0855d934e382c7ab632edc363370f27905aea65
Grzesiek1337 <gmiedzian@gmail.com>
2022-07-19 21:19:25 +0200

    added some changes

--- src/main/java/com/informations24h/informations24h/service/PostService.java
+++ src/main/java/com/informations24h/informations24h/service/PostService.java
@@ -1,6 +1,7 @@
 package com.informations24h.informations24h.service;
 
 import com.informations24h.informations24h.dto.PostDto;
+import com.informations24h.informations24h.model.Photo;
 import com.informations24h.informations24h.model.Post;
 import com.informations24h.informations24h.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +21,9 @@ public class PostService implements IPostService {
 
     @Autowired
     private final PostRepository postRepository;
+    @Autowired
+    private PhotoService photoService;
+
     @Autowired
     private ModelMapper modelMapper;
 
@@ -45,12 +50,25 @@ public class PostService implements IPostService {
 
     @Override
     public void delete(long id) {
-    postRepository.deleteById(id);
+        postRepository.deleteById(id);
+    }
+
+    public void saveImage(MultipartFile imageFile, Photo photo) throws Exception {
+        photoService.savePostPhoto(photo, imageFile);
+        photoService.save(photo);
+    }
+
+    public void removeMainImage(Post post) throws Exception {
+        photoService.removePhotoImage(post.getMainPostImage());
     }
 
     public List<PostDto> convertPostListToDto(List<Post> posts) {
         return posts.stream()
                 .map(p -> modelMapper.map(p, PostDto.class))
                 .collect(Collectors.toList());
+    }
+
+    public PostDto convertPostToDto(Post post) {
+        return modelMapper.map(post, PostDto.class);
     }
 }
